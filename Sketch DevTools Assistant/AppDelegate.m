e8d89ec5f75aa98b8c7ae1fa50db503fb81f8c30
turbobabr <andrey.shakhmin@gmail.com>
2014-10-08 23:22:28 +0900

    Moved SDTProtocolHandler from devtools and implemented all the handlers with a new "skdt" prefix. Didn't test them all.

--- Sketch DevTools Assistant/AppDelegate.m
+++ Sketch DevTools Assistant/AppDelegate.m
@@ -7,8 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import "SDTProtocolHandler.h"
 
 @interface AppDelegate ()
+@property (unsafe_unretained) IBOutlet NSTextView *logView;
 
 @property (weak) IBOutlet NSWindow *window;
 @end
@@ -16,9 +18,52 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-    // Sample change.
     
+    self.logView.string=@"Hello!";
+    
+}
+
+-(void)applicationWillFinishLaunching:(NSNotification *)notification {
+    [[NSAppleEventManager sharedAppleEventManager] setEventHandler:self andSelector:@selector(getURL:withReplyEvent:) forEventClass:kInternetEventClass andEventID:kAEGetURL];
+}
+
+- (void)getURL:(NSAppleEventDescriptor *)event withReplyEvent:(NSAppleEventDescriptor *)reply
+{
+    NSURL* url=[NSURL URLWithString:[[event paramDescriptorForKeyword:keyDirectObject] stringValue]];
+    if(url && [url.host isEqualToString:@"open"]) {
+        
+        
+        [self logEvent:url];
+        
+        NSArray* parts=[url.query componentsSeparatedByString:@"&"];
+        NSMutableDictionary* params=[NSMutableDictionary dictionary];
+        for(NSString* part in parts) {
+            NSArray* param=[part componentsSeparatedByString:@"="];
+            if(param.count==2) {
+                params[param[0]]=param[1];
+            }
+        }
+        
+        if(params[@"url"]) {
+            NSDictionary* schemeMap =
+            @{
+              @"skdttmate": @"textmate",
+              @"skdtwstorm": @"webstorm",
+              @"skdtappcode": @"appcode",
+              @"skdtsubl": @"sublime",
+              @"skdtatom": @"atom",
+              @"skdtxcode": @"xcode",
+              @"skdtatom": @"atom",
+              @"skdtmvim": @"macvim",
+              };
+            
+            [SDTProtocolHandler openFile:[[NSURL URLWithString:params[@"url"]] path] withIDE:schemeMap[url.scheme] atLine:[params[@"line"] integerValue]];
+        }
+    }
+}
+
+-(void)logEvent:(NSURL*)url {
+    self.logView.string=[NSString stringWithFormat:@"%@\n%@",self.logView.string,url];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
