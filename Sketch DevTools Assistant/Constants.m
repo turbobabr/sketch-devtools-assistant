2e2c556fd629f10ce6baac18cea384d60ea91b6e
turbobabr <andrey.shakhmin@gmail.com>
2014-10-17 21:31:10 +0900

    Minor clean-up

--- Sketch DevTools Assistant/Constants.m
+++ Sketch DevTools Assistant/Constants.m
@@ -6,9 +6,9 @@
 //  Copyright (c) 2014 Turbobabr. All rights reserved.
 //
 
-
-
 #import <Foundation/Foundation.h>
+
+
 NSString *const kPluginFolderPathSandboxed = @"~/Library/Containers/com.bohemiancoding.sketch3/Data/Library/Application Support/com.bohemiancoding.sketch3/Plugins/";
 NSString *const kPluginFolderPathBetaSandboxed = @"~/Library/Containers/com.bohemiancoding.sketch3.beta/Data/Library/Application Support/com.bohemiancoding.sketch3/Plugins/";
 NSString *const kPluginFolderPathNormal = @"~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/";
